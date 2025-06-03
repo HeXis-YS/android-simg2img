2e7076c94cc9c5b2afb72186601ad592115bc3d4
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-06-03 23:25:14 +0800

    2

--- simg2img.c
+++ simg2img.c
@@ -70,13 +70,13 @@ void usage() {
     fprintf(stderr, "Usage: simg2img [sparse_image_file] [raw_image_file]\n");
 }
 
-static ssize_t read_all(int fd, void *buf, size_t len) {
+static ssize_t rw_all(int fd, void *buf, size_t len, ssize_t (*io_func)(int, void *, size_t)) {
     size_t remaining = len;
     ssize_t ret;
     char *ptr = buf;
 
     while (remaining) {
-        ret = read(fd, ptr, remaining);
+        ret = io_func(fd, ptr, remaining);
         if (ret <= 0) {
             break;
         }
@@ -91,69 +91,57 @@ static ssize_t read_all(int fd, void *buf, size_t len) {
     return (ssize_t)(len - remaining);
 }
 
-static ssize_t write_all(int fd, const void *buf, size_t len) {
-    size_t remaining = len;
-    ssize_t ret;
-    char *ptr = (char *)buf;
-
-    while (remaining) {
-        ret = write(fd, ptr, remaining);
-        if (ret <= 0) {
-            break;
-        }
-        ptr += ret;
-        remaining -= ret;
-    }
-
-    if (ret < 0) {
-        return ret;
-    }
-
-    return (ssize_t)(len - remaining);
+static inline ssize_t read_all(int fd, void *buf, size_t len) {
+    return rw_all(fd, buf, len, &read);
 }
 
-static ssize_t skip_input(int fd, size_t len) {
-    size_t remaining = len;
-    ssize_t ret;
-
-    if (lseek64(fd, len, SEEK_CUR) >= 0) {
-        return (ssize_t)len;
-    }
-
-    while (remaining) {
-        ret = read_all(fd, copybuf, min(remaining, BUF_SIZE));
-        if (ret < 0) {
-            perror("Could not seek or read to skip input data");
-            exit(-1);
-        }
-        remaining -= ret;
-    }
-
-    return (ssize_t)len;
+static inline ssize_t write_all(int fd, const void *buf, size_t len) {
+    return rw_all(fd, buf, len, &write);
 }
 
-static int64_t skip_output(int fd, uint64_t len) {
+static int64_t skip_io(int fd, uint64_t len, int io) {
     uint64_t remaining = len;
     ssize_t ret;
+    uint8_t *buf;
+    ssize_t (*io_func)(int, void *, size_t);
 
     if (lseek64(fd, (off_t)len, SEEK_CUR) >= 0) {
         return len;
     }
 
-    if (*(uint32_t *)fillbuf != 0) {
-        memset(fillbuf, 0, BUF_SIZE);
+    if (io) {
+        buf = copybuf;
+        io_func = &read;
+    } else {
+        buf = fillbuf;
+        io_func = &write;
+        if (*(uint32_t *)buf != 0) {
+            memset(buf, 0, BUF_SIZE);
+        }
     }
 
     while (remaining) {
-        ret = write_all(fd, fillbuf, (size_t)min(remaining, BUF_SIZE));
+        ret = rw_all(fd, buf, (size_t)min(remaining, BUF_SIZE), io_func);
         if (ret < 0) {
-            perror("Could not seek or write to skip output data");
+            if (io) {
+                perror("Could not seek or read to skip input data");
+            } else {
+                perror("Could not seek or write to skip output data");
+            }
             exit(-1);
         }
         remaining -= (uint64_t)ret;
     }
 
     return (int64_t)len;
+}
+
+static inline ssize_t skip_input(int fd, size_t len) {
+    return (ssize_t)skip_io(fd, (uint64_t)len, 1);
+}
+
+static inline int64_t skip_output(int fd, uint64_t len) {
+    return skip_io(fd, len, 0);
 }
 
 int process_raw_chunk(int in, int out, uint32_t blocks, uint32_t blk_sz) {
@@ -185,11 +173,10 @@ uint32_t process_fill_chunk(int in, int out, uint32_t blocks, uint32_t blk_sz) {
     ssize_t ret;
     size_t chunk;
     uint32_t fill_val;
-    uint32_t *fillbuf32;
+    uint32_t *fillbuf32 = (uint32_t *)fillbuf;
 
     /* Fill fillbuf with the fill value */
     ret = read_all(in, &fill_val, sizeof(fill_val));
-    fillbuf32 = (uint32_t *)fillbuf;
     if (*fillbuf32 != fill_val) {
         for (int i = 0; i < (BUF_SIZE / sizeof(fill_val)); i++) {
             fillbuf32[i] = fill_val;
@@ -342,11 +329,13 @@ int main(int argc, char *argv[]) {
      */
     if (ftruncate64(out, (uint64_t)total_blocks * sparse_header.blk_sz)) {
         fprintf(stderr, "Error calling ftruncate() to set the image size\n");
-        exit(-1);
     }
 
     close(in);
     close(out);
+
+    free(copybuf);
+    free(fillbuf);
 
     if (sparse_header.total_blks != total_blocks) {
         fprintf(stderr, "Wrote %d blocks, expected to write %d blocks\n",
